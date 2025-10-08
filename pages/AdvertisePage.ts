76430d44aea25dc9e61373e7ef73ae4e38a4ccb2
N <2438063@cognizant.com>
2025-10-08 23:19:06 +0530

    advertise functionality tested

--- pages/AdvertisePage.ts
+++ pages/AdvertisePage.ts
@@ -1,26 +1,26 @@
-// import { Page } from '@playwright/test';
-// import { BasePage } from './BasePage';
-// import locators from '../locators/locators.json';
+import { Page } from '@playwright/test';
+import { BasePage } from './BasePage';
+import locators from '../locators/locators.json';
 
-// export class AdvertisePage extends BasePage {
-//   constructor(page: Page) {
-//     super(page);
-//   }
+export class AdvertisePage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
 
-//   async navigateToAdvertiseSection() {
-//     await this.page.goto('https://www.grotal.com/');
-//     await this.page.getByRole('link', { name: 'Add Your Business' }).click();
-//   }
+  async navigateToAdvertiseSection() {
+    await this.page.goto('https://www.grotal.com/');
+    await this.page.getByRole('link', { name: 'Add Your Business' }).click();
+  }
 
-//   async downloadAdvertisementManuals(): Promise<void> {
-//     const popupPromise = this.page.waitForEvent('popup');
-//     await this.page.getByRole('link', { name: 'PDF' }).click();
-//     const popup = await popupPromise;
+  async downloadAdvertisementManuals(): Promise<void> {
+    const popupPromise = this.page.waitForEvent('popup');
+    await this.page.getByRole('link', { name: 'PDF' }).click();
+    const popup = await popupPromise;
 
-//     const downloadPromise = this.page.waitForEvent('download');
-//     await this.page.getByRole('link', { name: 'PPT' }).click();
-//     const download = await downloadPromise;
+    const downloadPromise = this.page.waitForEvent('download');
+    await this.page.getByRole('link', { name: 'PPT' }).click();
+    const download = await downloadPromise;
 
-//     console.log(' Download triggered for:', await download.suggestedFilename());
-//   }
-// }
+    console.log(' Download triggered for:', await download.suggestedFilename());
+  }
+}
