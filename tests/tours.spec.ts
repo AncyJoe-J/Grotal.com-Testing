a0d856c27f6d5158e3626112e4bfb723ccc87d4b
Renuga-Suresh20 <renuga.s2@cognizant.com>
2025-10-08 23:46:05 +0530

    updated

--- tests/tours.spec.ts
+++ tests/tours.spec.ts
@@ -1,14 +1,14 @@
-import { test } from '../fixtures/page_fixtures';
+// import { test } from '../fixtures/page_fixtures';
  
-test('Validate Tours and Travels submenu items', async ({ pages }) => {
-  const expectedItems = [
-    'Travel Agents',
-    'Family Tour Packages',
-    'International Tour Packages',
-    'Domestic Tour Packages',
-    'Honeymoon Tour Packages'
-  ];
+// test('Validate Tours and Travels submenu items', async ({ pages }) => {
+//   const expectedItems = [
+//     'Travel Agents',
+//     'Family Tour Packages',
+//     'International Tour Packages',
+//     'Domestic Tour Packages',
+//     'Honeymoon Tour Packages'
+//   ];
  
-  await pages.tours.openToursAndTravelsCategory();
-  await pages.tours.validateSubmenuItems(expectedItems);
-});
+//   await pages.tours.openToursAndTravelsCategory();
+//   await pages.tours.validateSubmenuItems(expectedItems);
+// });
