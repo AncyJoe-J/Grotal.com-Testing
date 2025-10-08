07001fe91b258efc1a2a734e14ff26de3335ee17
Jose <2438066@cognizant.com>
2025-10-08 23:50:10 +0530

    tours testing implemented

--- pages/ToursPage.ts
+++ pages/ToursPage.ts
@@ -1,21 +1,21 @@
-// import { Page, expect } from '@playwright/test';
-// import { BasePage } from './BasePage';
-// import locators from '../locators/locators.json';
+import { Page, expect } from '@playwright/test';
+import { BasePage } from './BasePage';
+import locators from '../locators/locators.json';
  
-// export class ToursPage extends BasePage {
-//   constructor(page: Page) {
-//     super(page);
-//   }
+export class ToursPage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
  
-//   async openToursAndTravelsCategory() {
-//     await this.page.goto('https://www.grotal.com/');
-//     await this.page.locator(locators.tours.category).click();
-//     await this.page.locator(locators.tours.submenuContainer).scrollIntoViewIfNeeded();
-//   }
+  async openToursAndTravelsCategory() {
+    await this.page.goto('https://www.grotal.com/');
+    await this.page.locator(locators.tours.category).click();
+    await this.page.locator(locators.tours.submenuContainer).scrollIntoViewIfNeeded();
+  }
  
-//   async validateSubmenuItems(expectedItems: string[]) {
-//     for (const item of expectedItems) {
-//       await expect(this.page.getByText(item)).toBeVisible();
-//     }
-//   }
-// }
+  async validateSubmenuItems(expectedItems: string[]) {
+    for (const item of expectedItems) {
+      await expect(this.page.getByText(item)).toBeVisible();
+    }
+  }
+}
