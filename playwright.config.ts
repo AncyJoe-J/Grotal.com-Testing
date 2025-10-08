d0e8b8b1e9619a0e00b6fe5d822ba01b1aa4939f
Renuga-Suresh20 <renuga.s2@cognizant.com>
2025-10-08 23:34:42 +0530

    playwright config file updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,12 +26,17 @@ export default defineConfig({
   reporter: [['allure-playwright']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+  // use: {
+  //   /* Base URL to use in actions like `await page.goto('/')`. */
+  //   // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+  //   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  //   trace: 'on-first-retry',
+  // },
+  use: {
+    screenshot: 'on',
+    video: 'on',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -39,17 +44,17 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+     }
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
